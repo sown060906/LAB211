b9a770b6e09c1628618f17e8e2a5d2946268752f
son <quyson6dltt@gmail.com>
2025-10-29 07:23:02 +0700

    Add files via upload

---
+++ P0073/Expense.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package J1.S.P0073;
+
+/**
+ *
+ * @author Admin
+ */
+public class Expense {
+    
+}
---
+++ P0073/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package J1.S.P0073;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        ExpenseManager manager = new ExpenseManager();
+        manager.displayMenu();
+    }
+}
