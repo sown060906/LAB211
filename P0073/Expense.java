27f9980e0e05d6aa5c8092f34227e1ddd42d83ea
son <quyson6dltt@gmail.com>
2025-10-29 10:07:08 +0700

    Add fields and constructor to Expense class

--- P0073/Expense.java
+++ P0073/Expense.java
@@ -9,5 +9,32 @@ package J1.S.P0073;
  * @author Admin
  */
 public class Expense {
-    
+    private int id;
+    private String date;
+    private double amount;
+    private String content;
+
+    public Expense(int id, String date, double amount, String content) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
+
