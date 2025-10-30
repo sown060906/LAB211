4c135a79bed1f4980b6a2510a197dc97d9646971
son <quyson6dltt@gmail.com>
2025-10-31 01:12:27 +0700

    Implement input validation methods in Validation class

--- P0073/Validation.java
+++ P0073/Validation.java
@@ -8,6 +8,77 @@ package J1.S.P0073;
  *
  * @author Admin
  */
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Validation {
-    
+    private static final Scanner in = new Scanner(System.in);
+
+    public static int checkIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int choice = Integer.parseInt(in.nextLine());
+                if (choice < min || choice > max) {
+                    throw new NumberFormatException();
+                }
+                return choice;
+            } catch (NumberFormatException ex) {
+                System.err.println("Re-input");
+            }
+        }
+    }
+
+    public static String checkInputString() {
+        while (true) {
+            String result = in.nextLine().trim();
+            if (result.length() > 0) {
+                return result;
+            }
+            System.err.println("Input can't be empty");
+        }
+    }
+
+    public static int checkInputInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(in.nextLine());
+            } catch (NumberFormatException ex) {
+                System.err.println("Re-input");
+            }
+        }
+    }
+
+    public static double checkInputDouble() {
+        while (true) {
+            try {
+                double value = Double.parseDouble(in.nextLine().trim());
+                if (value <= 0) {
+                    System.err.println("Amount must be greater than 0");
+                    continue;
+                }
+                return value;
+            } catch (NumberFormatException ex) {
+                System.err.println("Re-input");
+            }
+        }
+    }
+
+    public static String checkInputDate() {
+        while (true) {
+            try {
+                String result = in.nextLine().trim();
+                SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+                format.setLenient(false);
+                Date date = format.parse(result);
+                if (result.equals(format.format(date))) {
+                    return result;
+                }
+            } catch (ParseException ex) {
+            }
+            System.err.println("Re-input (format: dd-MMM-yyyy, e.g., 11-Apr-2009)");
+        }
+    }
 }
+
